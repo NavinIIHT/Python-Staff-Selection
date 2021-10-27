343c17324518f69336b468a3c91effb2a82eba5f
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2021-10-27 10:46:19 +0530

    Add files via upload

---
+++ test/test_functional.py
@@ -0,0 +1,51 @@
+# import colorama
+# from colorama import Fore
+# colorama.init(autoreset=True)
+import unittest
+import sys
+sys.path.append("..")
+from mainclass import MainClass
+class FuctionalTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        with open("output_revised.txt","w") as f:
+            pass
+    def test_is_candidate_qualified(self):
+        obj=MainClass.calculate_result("Venu,92,98,96")
+        if type(obj)!=type(None):
+            with open("output_revised.txt","a") as f:
+                f.write("TestIsCandidateQualified=True\n")
+                # print("{}TestCaseTestIsCandidateQualified = {}Passed".format(Fore.YELLOW,Fore.GREEN))
+                print("TestIsCandidateQualified = Passed")
+        else:
+            with open("output_revised.txt","a") as f:
+                f.write("TestIsCandidateQualified=False\n")
+                # print("{}TestIsCandidateQualified = {}Failed".format(Fore.YELLOW,Fore.RED))
+                print("TestIsCandidateQualified = Failed")
+    def test_is_candidate_fail(self):
+        obj=MainClass.calculate_result("Ravan,42,38,96")
+        #print(type(obj),type(None))
+        if type(obj)==type(None):
+            with open("output_revised.txt","a") as f:
+                f.write("TestIsCandidateFail=True\n")
+                # print("{}TestIsCandidateFail = {}Passed".format(Fore.YELLOW,Fore.GREEN))
+                print("TestIsCandidateFail = Passed")
+        else:
+            with open("output_revised.txt","a") as f:
+                f.write("TestIsCandidateFail=False\n")
+                # print("{}TestIsCandidateFail = {}Failed".format(Fore.YELLOW,Fore.RED))
+                print("TestIsCandidateFail = False")
+
+    def test_is_candidate_pass(self):
+        obj=MainClass.calculate_result("Ravan,52,55,58")
+        #print(type(obj),type(None))
+        if type(obj)==type(None):
+            with open("output_revised.txt","a") as f:
+                f.write("TestIsCandidatePass=True\n")
+                # print("{}TestIsCandidatePass = {}Passed".format(Fore.YELLOW,Fore.GREEN))
+                print("TestIsCandidatePass = Passed")
+        else:
+            with open("output_revised.txt","a") as f:
+                f.write("TestIsCandidatePass=False\n")
+                # print("{}TestIsCandidatePass = {}Failed".format(Fore.YELLOW,Fore.RED))
+                print("TestIsCandidatePass = False")
