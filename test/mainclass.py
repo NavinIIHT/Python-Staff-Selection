f743ab8eb548cab7c53a12b9664d813252b54d35
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2021-10-27 10:47:30 +0530

    Add files via upload

---
+++ test/mainclass.py
@@ -0,0 +1,13 @@
+
+class MainClass:
+    @staticmethod
+    def calculate_result(app_data):
+        pass
+
+    @staticmethod
+    def sort_result(all_app_result):
+        pass
+
+
+if __name__=="__main__":
+    pass
